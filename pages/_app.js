591afe9203f42b7bf52ff5285c938bf7dca8ff84
TheBoringDude <iamcoderx@gmail.com>
2021-01-18 07:37:37 +0800

    Try to setup GTAG Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 import '../style/tailwind.css'
 
 function AddieProductions({ Component, pageProps }) {
+  // initialize GTAG Manager
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NRQRHP2' })
+  }, [])
+
   return <Component {...pageProps} />
 }
 
